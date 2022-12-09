d8a3d41e307f0d1dfd842da446ebeac6895bae1a
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2022-12-09 13:26:36 +0530

    hamburger close fixes

--- dypis/src/app/nav-bar/nav-bar.component.ts
+++ dypis/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { menuItems } from '../utils/data';
 import { hamburgerSildingAnimation } from './hamburger-animation';
@@ -14,4 +14,25 @@ export class NavBarComponent {
   barsIcon = faBars;
   closeIcon = faXmark;
   showHamburgerMenu = false;
+
+  @HostListener('window:click', ['$event'])
+  closeHamburger(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('#hamburger-menu') && this.showHamburgerMenu) {
+      this.showHamburgerMenu = false;
+    }
+  }
+
+  @HostListener('document:keydown', ['$event']) closeHamburgerOnEscape(
+    event: KeyboardEvent
+  ) {
+    if (event.code === 'Escape' && this.showHamburgerMenu) {
+      this.showHamburgerMenu = false;
+    }
+  }
+
+  openHamburgerMenu(event: Event): void {
+    event.stopPropagation();
+    this.showHamburgerMenu = true;
+  }
 }
