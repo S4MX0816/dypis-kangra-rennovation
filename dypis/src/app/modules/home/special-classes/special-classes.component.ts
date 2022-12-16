914803965e95c42af5dff5e0bbfcea4a24933ff2
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2022-12-17 00:50:08 +0530

    cards animation

--- dypis/src/app/modules/home/special-classes/special-classes.component.ts
+++ dypis/src/app/modules/home/special-classes/special-classes.component.ts
@@ -12,15 +12,32 @@ import { updateAnimationStateOnScroll } from 'src/app/utils/helpers';
 })
 export class SpecialClassesComponent {
   specialClasses = specialClasses.slice();
-
   updateAnimationStateOnScroll = updateAnimationStateOnScroll;
   state = 'start';
+  translatePos = 0;
+  offsetPos = 0;
+  readonly TRANSLATE_BY = 100;
+  readonly OFFSET_ADD = 24;
+  readonly REMAINING_CARDS = 5;
+  readonly TRANSITION_TIMING = 3000; // in milliseconds
 
   constructor() {
     this.applyTransition();
   }
 
   applyTransition() {
-    setInterval(() => {});
+    setInterval(() => {
+      this.translatePos -= this.TRANSLATE_BY;
+      this.offsetPos += this.OFFSET_ADD;
+      document.querySelectorAll('.special-class-card').forEach((el) => {
+        const divEl = el as HTMLDivElement;
+        divEl.style.transform = `translateX(calc(${this.translatePos}% - ${this.offsetPos}px))`;
+      });
+      const arrayLength = this.specialClasses.length;
+      const iterationNumber = this.offsetPos / this.OFFSET_ADD;
+      if (arrayLength - iterationNumber <= this.REMAINING_CARDS) {
+        this.specialClasses.push(...specialClasses);
+      }
+    }, this.TRANSITION_TIMING);
   }
 }
