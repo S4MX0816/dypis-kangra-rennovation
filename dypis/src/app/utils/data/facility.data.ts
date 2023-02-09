3db06e55c641cb38845b119b60c5b9fb7cc4eb7a
ArtisticTrehan <124918519+ArtisticTrehan@users.noreply.github.com>
2023-02-09 19:19:16 +0000

    please accept my changes

--- dypis/src/app/utils/data/facility.data.ts
+++ dypis/src/app/utils/data/facility.data.ts
@@ -3,32 +3,32 @@ import { BasicCard } from '../../models/interfaces';
 export const facilities: BasicCard[] = [
   {
     name: 'school bus',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    text: 'DYPIS provides safe and reliable transportation to students who wish to avail this facility. The students are accompanied by well-trained attendants to ensure their safety.',
     image: '/assets/images/bus.png',
   },
   {
-    name: 'custom food',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    name: 'Food Facility',
+    text: 'We have provision of Hygienic, Nutritious diet for students. School provides lunch (if opted) during school hours.',
     image: '/assets/images/french-fries.png',
   },
   {
     name: 'science labs',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    text: 'Our labs are well equipped with the latest infrastructure that helps students to develop a deep understanding of the topic',
     image: '/assets/images/bulb.png',
   },
   {
     name: 'world classes',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    text: 'School has Technology Enabled Classrooms equipped with Interactive Projectors to make the day-to-day teaching learning process interactive, interesting and effective.',
     image: '/assets/images/pencil.png',
   },
   {
-    name: 'funny games',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    name: 'School Infrastructure',
+    text: 'We provide infrastructure related to educational, physical and friendly environment for overall development of child.',
     image: '/assets/images/book.png',
   },
   {
-    name: 'great facilities',
-    text: 'Li Europan lingues es membres del sam familie. Lor separat existentie es un',
+    name: 'Achievements',
+    text: 'School has been a recipient of many awards. Our students have recently won awards for different events in District and State Level Tournaments.',
     image: '/assets/images/trophy.png',
   },
 ];
