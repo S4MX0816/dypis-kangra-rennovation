e63da8c9ece6d8c4b899b526d52e87af1e1250db
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-09 13:09:12 +0530

    doc name updates

--- dypis/src/app/utils/data/docs.data.ts
+++ dypis/src/app/utils/data/docs.data.ts
@@ -1,9 +1,9 @@
 export const docs = [
   'registration-certificate',
-  'pta-committee',
+  'PTA-committee',
   'management-committee',
   'certificate-of-land',
   'permission-of-institution',
   'recognition-certificate',
-  'no-objection-certificate',
+  'NOC-fire-services',
 ];
--- dypis/src/app/utils/docs-name.pipe.ts
+++ dypis/src/app/utils/docs-name.pipe.ts
@@ -7,9 +7,6 @@ export class DocsNamePipe implements PipeTransform {
   transform(value: string): string {
     const arr = value.split('-');
     const formattedVal = arr.reduce((acc, val) => {
-      if (val === 'pta') {
-        return acc + 'PTA' + ' ';
-      }
       return acc + val.at(0)?.toUpperCase() + val.substring(1) + ' ';
     }, '');
     return formattedVal.trim();
