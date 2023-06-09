489dae6a07149a97150b133f99c7b2fb745fb3e9
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-09 13:19:33 +0530

    loader logic updated

--- dypis/src/app/loader/loader.service.ts
+++ dypis/src/app/loader/loader.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class LoaderService {
-  isLoading$ = new Subject<boolean>();
+  isLoading$ = new BehaviorSubject<boolean>(true);
   totalRequests = 0;
 
   show() {
