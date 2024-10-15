9ad6f2714ff33a6cf6dc5e14ad08a842957b3fcb
Sanyam Trehan <trehansanyam@gmail.com>
2024-10-15 15:40:53 +0530

    feat: update faculty

--- dypis/src/app/utils/data/faculty.data.ts
+++ dypis/src/app/utils/data/faculty.data.ts
@@ -5,128 +5,151 @@ export const faculty: Faculty[] = [
     employeeCode: 22,
     name: 'Arun Martin Chauhan',
     designation: 'Principal',
-    qualification: 'PHD',
-  },
-  {
-    employeeCode: 17,
-    name: 'Agam Sharma',
-    designation: 'Managing Director',
+    qualification: 'M.A English, P.G.Dip TESOL, B.Ed.',
   },
   {
     employeeCode: 8,
     name: 'Achal Trehan',
     designation: 'Teacher',
-    qualification: 'MCA, PGDCA, BCA',
+    qualification: 'MCA, BCA, PGDCA, ADHNT',
   },
   {
     employeeCode: 54,
     name: 'Ambika Sharma',
     designation: 'Teacher',
+    qualification: 'M. A Sanskrit and Public Administration B. Ed. (TET)',
   },
   {
     employeeCode: 19,
     name: 'Anita Dhiman',
     designation: 'Coordinator',
+    qualification: 'MA Hindi,B.Ed(TET-TGT & LT), PGDCA',
   },
   {
     employeeCode: 21,
     name: 'Anita Dhiman',
-    designation: 'Teacher',
+    designation: 'Coordinator',
+    qualification: 'M.SC Biochemistry, PGD -Microbiology, B.Ed,Ctet',
   },
   {
     employeeCode: 55,
     name: 'Arti Sharma',
     designation: 'Teacher',
+    qualification: 'M.A (Hindi) B.Ed (TET)',
   },
   {
     employeeCode: 58,
     name: 'Ashna Bhardwaj',
     designation: 'Teacher',
+    qualification: 'M.Sc(Chemistry) B.Ed',
   },
   {
     employeeCode: 12,
     name: 'Gunjan Dogra',
-    designation: 'Teacher',
+    designation: 'Coordinator',
+    qualification: 'B.ED,MA English ,PDPET',
   },
   {
     employeeCode: 25,
     name: 'Mahesh Kumar',
     designation: 'Teacher',
+    qualification:
+      'B.P.E, BP. ED, P.G Yog, M.A.physical Education, Nis Basketball',
   },
   {
     employeeCode: 31,
     name: 'Nandita Mahajan',
     designation: 'Teacher',
+    qualification: 'B.sc Medical, M.sc Zoology, B.ed',
   },
   {
     employeeCode: 35,
     name: 'Nitisha Sharma',
     designation: 'Teacher',
+    qualification: 'Bsc.IT,Msc.IT,NTT',
   },
   {
     employeeCode: 14,
     name: 'Priyanka Gupta',
     designation: 'Teacher',
+    qualification: 'MA English,B.Ed',
   },
   {
     employeeCode: 24,
     name: 'Rajesh Puri',
     designation: 'Teacher',
+    qualification: 'MSC ( Maths) , B.ed, HPTET',
   },
   {
     employeeCode: 23,
     name: 'Reeta Singh',
     designation: 'Teacher',
+    qualification: 'M.A ( Mathematics) B.Ed',
   },
   {
     employeeCode: 63,
     name: 'Robina Rana',
     designation: 'Teacher',
+    qualification:
+      'B.Sc.(Non Medical) , M.Sc.Mathematics , M.A. Education, D.El.Ed , B.Ed , CTET',
   },
   {
     employeeCode: 49,
     name: 'Sandeep Singh',
-    designation: 'Administrator',
+    designation: 'Counsellor',
+    qualification:
+      'B.A (Psychology), B.Ed., DCA, PG Diploma in Child Psychology and Parenting(Pursuing), HPTET',
   },
   {
     employeeCode: 18,
     name: 'Shiwani Puri',
     designation: 'Teacher',
+    qualification: 'M.Sc (Botany), B.Ed, B.Sc (Medical)',
   },
   {
     employeeCode: 92,
     name: 'Sorabh Kumar',
     designation: 'Teacher',
+    qualification: 'B.A (sociology), M.A ( sociology pursuing)',
   },
   {
     employeeCode: 107,
     name: 'Vinay Madhuwal',
     designation: 'Administrator',
+    qualification: 'B.Sc.(Non - Medical), B.Ed., M.A.(History), HPTET',
   },
   {
     employeeCode: 52,
     name: 'Viplove Thakur',
     designation: 'Teacher',
+    qualification:
+      'MA (English) ,B. Ed, PG Diploma in Mass Communication, CTET, HPTET',
   },
   {
     employeeCode: 102,
     name: 'Mr. Abhishek',
     designation: 'Teacher',
+    qualification: 'B.A ( Geography), EEE(English),M.A (Geography Pursue￼)',
   },
   {
     employeeCode: 91,
     name: 'Mr. Akshay Kumar',
     designation: 'Teacher',
+    qualification:
+      'Sangeet Bhushan, Sangeet visharad (Sitar) MA ( Instrumental Music)',
   },
   {
     employeeCode: 75,
     name: 'Mr. Happy',
-    designation: 'Administrator',
+    designation: 'Accountant',
+    qualification: 'B.com , M.com , Diploma ( Accountancy & Taxation)',
   },
   {
     employeeCode: 112,
     name: 'Mr. Himanshu Abrol',
     designation: 'Teacher',
+    qualification:
+      'B.A (German and English), Certificate, diploma and advance diploma in German Language, Pursuing M.A. in German language',
   },
   {
     employeeCode: 83,
@@ -134,14 +157,10 @@ export const faculty: Faculty[] = [
     designation: 'Teacher',
   },
   {
-    employeeCode: 81,
-    name: 'Mr. Navneet Kumar',
-    designation: 'Teacher',
-  },
-  {
     employeeCode: 103,
     name: 'Mr. Neeraj Kumar',
     designation: 'Teacher',
+    qualification: 'B.P.ED, M.P.ED, NIS DIPLOMA IN FOOTBALL,D LICENSE.',
   },
   {
     employeeCode: 113,
@@ -152,36 +171,46 @@ export const faculty: Faculty[] = [
     employeeCode: 109,
     name: 'Mr. Vinod Kumar',
     designation: 'Teacher',
+    qualification: 'B.A, PGDCA, BP.ED. Certificate course in NIS Badminton',
   },
   {
     employeeCode: 80,
     name: 'Mr. Yogesh Kumar',
     designation: 'Teacher',
+    qualification:
+      'B.Sc (MATH ) , MA (English), PGDCA , VISHARAD in dance ( Pursuing )',
   },
   {
     employeeCode: 79,
     name: 'Mrs. Asha Kaushal',
     designation: 'Teacher',
+    qualification: 'M.sc Botany , B.Ed (Medical), CTET(1&2) ,HPTET &NTT.',
   },
   {
     employeeCode: 105,
     name: 'Mrs. Bhawna',
     designation: 'Teacher',
+    qualification: 'M.A( Political science),B.Ed(TET),PGDCA',
   },
   {
     employeeCode: 11,
     name: 'Mrs. Isha Bhasin',
     designation: 'Teacher',
+    qualification:
+      'B.A with computer science,B.ED,PGDCA,MSC.IT science,MCA,Digital Marketing,Ethical hacking,web developer,AI',
   },
   {
     employeeCode: 84,
     name: 'Mrs. KM Ruchi',
     designation: 'Teacher',
+    qualification: 'B.sc sports, BPED, Certificate course in NIS FENCING',
   },
   {
     employeeCode: 37,
     name: 'Mrs. Kritika Joshi',
     designation: 'Teacher',
+    qualification:
+      'B.Sc (Medical) , B.Ed , PG diploma in Special education ( ADHD , Autism and learning disabilities) , Certificate in counselling for teachers and Certificate in Teaching.',
   },
   {
     employeeCode: 87,
@@ -192,36 +221,44 @@ export const faculty: Faculty[] = [
     employeeCode: 76,
     name: 'Mrs. Mamta Dogra',
     designation: 'Teacher',
+    qualification: 'M.A (pol. science ),PGDCA,D.El.Ed.(Tet)',
   },
   {
     employeeCode: 77,
     name: 'Mrs. Nidhi Dodain',
     designation: 'Teacher',
+    qualification: 'BA ( Geography) 2 yrs NTT',
   },
   {
     employeeCode: 120,
     name: 'Mrs. Nitika Sharma',
     designation: 'Teacher',
+    qualification: 'BBA,MBA( masters in Human Resources),NTT',
   },
   {
     employeeCode: 88,
     name: 'Mrs. Pooja Gupta',
     designation: 'Teacher',
+    qualification:
+      'M.A. (Eng), M.Ed., D.El.Ed, TET (Primary and TGT) , CTET (Primary and TGT)',
   },
   {
     employeeCode: 78,
     name: 'Mrs. Reena Raina',
     designation: 'Teacher',
+    qualification: 'M.A(Political Science),NTT, L.T(Prabhakar)',
   },
   {
     employeeCode: 67,
     name: 'Mrs. Renu Sharma',
     designation: 'Teacher',
+    qualification: 'M.A.(Hindi),B.Ed,TET(L.T) ,PGDCA.',
   },
   {
     employeeCode: 114,
     name: 'Mrs. Robina Martin Chauhan',
     designation: 'Teacher',
+    qualification: 'M.A. English, TESOL, B.Ed.',
   },
   {
     employeeCode: 90,
@@ -232,6 +269,7 @@ export const faculty: Faculty[] = [
     employeeCode: 96,
     name: 'Mrs. Simi Rana',
     designation: 'Coordinator',
+    qualification: 'M.Sc (Ecology and Environment),B.Ed',
   },
   {
     employeeCode: 70,
@@ -242,41 +280,50 @@ export const faculty: Faculty[] = [
     employeeCode: 45,
     name: 'Ms. Ekta Shandil',
     designation: 'Teacher',
+    qualification: 'MSc Geography, B.Ed',
   },
   {
     employeeCode: 122,
     name: 'Ms. Komal',
-    designation: 'Administrator',
+    designation: 'Receptionist',
+    qualification: 'MBA (Pursuing), BBA, Certification in Account Executive',
   },
   {
     employeeCode: 93,
     name: 'Ms. Kritika',
     designation: 'Teacher',
+    qualification:
+      'M.sc Environment science ( pursuing), B.sc ( Botany) ,D.El.Ed ,C-DAC',
   },
   {
     employeeCode: 99,
     name: 'Ms. Manvi Sharma',
     designation: 'Teacher',
+    qualification: 'B.Sc. (Non Med.), D.El.Ed',
   },
   {
     employeeCode: 69,
     name: 'Ms. Monika Chack',
     designation: 'Teacher',
+    qualification: 'NPTT, B.Comm Hons.,M.Comm, D.EL.ED,TET, CTET',
   },
   {
     employeeCode: 111,
     name: 'Ms. Mridula Mehta',
     designation: 'Teacher',
+    qualification: 'B. A. , B. Ed., NTT',
   },
   {
     employeeCode: 38,
     name: 'Ms. Neetu Behal',
     designation: 'Teacher',
+    qualification: 'B.Sc(medical),B.Ed,M.A.(Eng)',
   },
   {
     employeeCode: 115,
     name: 'Ms. Niyati',
     designation: 'Teacher',
+    qualification: 'M.Sc(Botany),B.Sc(Medical),B.Ed(TET)',
   },
   {
     employeeCode: 94,
@@ -287,20 +334,24 @@ export const faculty: Faculty[] = [
     employeeCode: 108,
     name: 'Ms. Sakshi Saini',
     designation: 'Teacher',
+    qualification: 'B.sc (Botany) , B.Ed , M.sc pursuing',
   },
   {
     employeeCode: 98,
     name: 'Ms. Sapna Chaudhary',
     designation: 'Teacher',
+    qualification: 'M.Sc Zoology ,B.Ed , HPTET ,CTET',
   },
   {
     employeeCode: 86,
     name: 'Ms. Shivali',
     designation: 'Teacher',
+    qualification: 'M.Sc Chemistry, B.Sc - Botany , B.Ed (TET)',
   },
   {
     employeeCode: 110,
     name: 'Ms. Sumedha Cheema',
     designation: 'Teacher',
+    qualification: 'M.Sc ( Mathematics), B.Ed',
   },
 ];
